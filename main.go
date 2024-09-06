b822455ee8ec14194c18e9b96d1ec3391bf6c4c6
y15 <lorenz.weiss@outlook.com>
2024-09-06 12:16:39 +0200

    playing around with go

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	conferenceName := "Best Conference"
+	const conferenceTickets = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("Welcome to %v \n", conferenceName)
+	fmt.Printf("We have total off %v tickets and still aviable: %v \n", conferenceTickets, remainingTickets)
+	fmt.Println("-- Get your tickets here --")
+
+	var bookings [50]string
+
+	var userName string
+
+	//Get Username
+	fmt.Println("What is you`re first name?")
+	fmt.Scan(&userName)
+
+	bookings[0] = userName
+
+	fmt.Printf("Hello %v", userName)
 }
